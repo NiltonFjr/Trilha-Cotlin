efd15aa98162c1700c3d904a5c843e6fdd58befc
NiltonFjr <107730965+NiltonFjr@users.noreply.github.com>
2022-07-04 19:54:25 -0300

    refatoraçao

--- Trilha Cotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Trilha Cotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,15 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Nilton"
-
     var cpf: String = "123.456.789-00"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = $nome e $cpf
 }
 
 fun main() {
-    val nilton = Pessoa()
-
-    println(nilton)
-    println(nilton.nome)
-    println(nilton.cpf)
+     val nilton = Pessoa()
+     println(nilton.pessoaInfo())
 }

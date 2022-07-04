16f8ced5fda1de9e630994960a418d4d5c137d79
NiltonFjr <107730965+NiltonFjr@users.noreply.github.com>
2022-07-04 19:45:59 -0300

    mudança de private set

--- Trilha Cotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Trilha Cotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,13 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Nilton"
-    var cpf: String = "123.456.789-00"
 
+    var cpf: String = "123.456.789-00"
+    private set
 }
 
 fun main() {
     val nilton = Pessoa()
 
+    println(nilton)
     println(nilton.nome)
     println(nilton.cpf)
 }

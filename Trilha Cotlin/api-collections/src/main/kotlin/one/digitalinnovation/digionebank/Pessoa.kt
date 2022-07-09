f3b1770a8ef5d12c5eb620261404730fcabc4811
NiltonFjr <107730965+NiltonFjr@users.noreply.github.com>
2022-07-08 23:53:28 -0300

    upgrade classe abstrata

--- Trilha Cotlin/api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Trilha Cotlin/api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Nilton"
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-
-}
-fun main() {
-    val nilton = Pessoa()
-
-    println(nilton.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
